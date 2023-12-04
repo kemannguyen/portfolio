1a57f5e54f888c6408a7d2b3c2898021d74bc51c
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-12-05 00:27:54 +0100

    Update projectdata.js

--- src/components/projects/projectdata.js
+++ src/components/projects/projectdata.js
@@ -29,23 +29,32 @@ export const ProjectDataFS = [
       "<br> ● Security with keycloak" +
       "<br> ● Assistance with Backend" +
       "<br> ● Assistance with Admin related map functionality" +
-      "<br><br> github"+
-      "<br> frontend:  <a href=https://github.com/Nirase/HvZCaseFrontend target=_blank><u>https://github.com/Nirase/HvZCaseFrontend</u></a>"+
+      "<br><br> github" +
+      "<br> frontend:  <a href=https://github.com/Nirase/HvZCaseFrontend target=_blank><u>https://github.com/Nirase/HvZCaseFrontend</u></a>" +
       "<br> backend:  <a href=https://github.com/Nirase/HvZCaseBackend target=_blank><u>https://github.com/Nirase/HvZCaseBackend</u></a>",
     githublink: "",
   },
   {
-    name: "Dundrian",
+    name: "Dundrian(Ongoing)",
     img: "https://i.imgur.com/ZHqUl6p.png",
     description:
       "This website is an hobby project where the user can play an turn-based online multiplayer game called Dundrian. The game is an party game filled with teamwork and deception where the objective of the majority is to kill the boss while other players want to sabotage the other players and help the boss to win." +
-      "<br><br>This game is based on an novel board game my group created during my studies at Malmö University. More info about the game can be found below"+
+      "<br><br>This game is based on an novel board game my group created during my studies at Malmö University. More info about the game can be found below" +
       "<br>Game GDD: <a href=https://drive.google.com/file/d/1FMqZtF7Ha-ClJKQ6NzDmh3VZHDZiztEH/view?usp=drive_link target=_blank>https://drive.google.com/file/d/1FMqZtF7Ha-ClJKQ6NzDmh3VZHDZiztEH/view?usp=drive_link</a>",
 
     githublink: "https://dundrian.vercel.app/",
   },
-];
+  {
+    name: "Skurup restaurang & grill(Ongoing)",
+    img: "https://i.imgur.com/ea37hSH.png",
+    description:
+      "This website is made for a restaurant whose sells home cooking and fast food. In the website the user can find all necessary information about the restaurant." +
+      "<br><br>Booking functionality isn't implemented since it isn't possible to book a table in the restaurant." +
+      "<br></a>",
 
+    githublink: "https://skurup-rog.vercel.app/",
+  },
+];
 
 export const ProjectDataGame = [
   {
@@ -97,8 +106,7 @@ export const ProjectDataIllustration = [
   {
     name: "Children's book illustration",
     img: "https://i.imgur.com/sfRKcXp.jpg",
-    description:
-      "Drew illustrations for a childrens book",
+    description: "Drew illustrations for a childrens book",
     githublink: "",
   },
   {
@@ -119,14 +127,14 @@ export const ProjectDataIllustration = [
     name: "Hobby art",
     img: "https://cdn.discordapp.com/attachments/671486842960478243/825848055668015124/sky.png",
     description:
-    "Illustrations I've created during the process of self improvement",
+      "Illustrations I've created during the process of self improvement",
     githublink: "",
   },
   {
     name: "Hobby art",
     img: "https://cdn.discordapp.com/attachments/671486842960478243/727656821489270835/mush_village.png",
     description:
-    "Illustrations I've created during the process of self improvement",
+      "Illustrations I've created during the process of self improvement",
     githublink: "",
   },
   {
